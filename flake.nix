a74c65abe7f9e26e93fdfcf57ed8b5c0c76f137e
Sridhar Ratnakumar <srid@srid.ca>
2023-07-24 14:37:37 -0400

    Fetch latest (outside of nix build)

--- flake.nix
+++ flake.nix
@@ -12,8 +12,19 @@
         ./osrm.nix
         ./docker.nix
       ];
-      perSystem = { self', pkgs, ... }: {
+      perSystem = { self', pkgs, lib, ... }: {
         formatter = pkgs.nixpkgs-fmt;
+
+        apps.fetch.program = pkgs.writeShellApplication {
+          name = "osrm-fetch";
+          text = ''
+            NAME=southern-zone-latest
+            set -x
+            rm -f $NAME.osm.pbf*
+            ${lib.getExe pkgs.wget} http://download.geofabrik.de/asia/india/$NAME.osm.pbf
+            nix hash file $NAME.osm.pbf  --base32 > $NAME.osm.pbf.hash
+          '';
+        };
       };
     };
 }
