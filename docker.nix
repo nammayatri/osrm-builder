6049a49a7664a05bdfe49f76fb9a4984ea0f01b8
saimanohar.veeravajhula <saimanohar.veeravajhula@juspay.in>
2023-07-24 23:28:28 +0530

    tag

--- docker.nix
+++ docker.nix
@@ -4,36 +4,28 @@ let
   imageName = "ghcr.io/nammayatri/osrm-builder";
   # self.rev will be non-null only when the working tree is clean
   # This is equivalent to `git rev-parse --short HEAD`
-  # imageTag = builtins.substring 0 6 (self.rev or "dev");
-  imageTag = "dev";
+  imageTag = builtins.substring 0 6 (self.rev or "dev");
 in
 {
     perSystem = { self', pkgs, lib, ... }: {
       packages = {
         dockerImage = pkgs.dockerTools.buildImage {
           name = imageName;
+          tag = imageTag;
           created = "now";
-          tag = "dev"; # TODO: based on date
           copyToRoot = pkgs.buildEnv {
+            name = "osrm-builder-contents";
             paths = with pkgs; [
               cacert
               coreutils
               bashInteractive
               self'.packages.osrm-data
             ];
-            name = "osrm-data-img";
             /* pathsToLink = [
               "/bin"
               "/opt"
             ];
             */
-          };
-          config = {
-            Env = [
-              "SSL_CERT_FILE=${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt"
-              # Ref: https://hackage.haskell.org/package/x509-system-1.6.7/docs/src/System.X509.Unix.html#getSystemCertificateStore
-              "SYSTEM_CERTIFICATE_PATH=${pkgs.cacert}/etc/ssl/certs/ca-bundle.crt"
-            ];
           };
         };
       };
