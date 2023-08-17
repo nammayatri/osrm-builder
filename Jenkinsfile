05fd2d9059481327663e293e556214693033af73
Sridhar Ratnakumar <srid@srid.ca>
2023-08-17 16:04:59 -0400

    fix

--- Jenkinsfile
+++ Jenkinsfile
@@ -6,7 +6,7 @@ pipeline {
     stages {
         stage ('Nix Build') {
             steps {
-                nixCI
+                nixCI()
             }
         }
         stage ('Docker image') {
--- docker.nix
+++ docker.nix
@@ -3,29 +3,29 @@ let
   imageTag = builtins.substring 0 6 (builtins.readFile ./SE-zone-latest.osm.pbf.hash);
 in
 {
-    perSystem = { self', pkgs, lib, ... }: {
-      packages = {
-        dockerImage = pkgs.dockerTools.buildImage {
-          name = imageName;
-          tag = imageTag;
-          created = "now";
-          copyToRoot = pkgs.buildEnv {
-            name = "osrm-data";
-            paths = with pkgs; [
-              cacert
-              coreutils
-              bashInteractive
-              self'.packages.osrm-data
-              self'.packages.osrm-server
-              osrm-backend
-            ];
-            /* pathsToLink = [
+  perSystem = { self', pkgs, lib, ... }: {
+    packages = {
+      dockerImage = pkgs.dockerTools.buildImage {
+        name = imageName;
+        tag = imageTag;
+        created = "now";
+        copyToRoot = pkgs.buildEnv {
+          name = "osrm-data";
+          paths = with pkgs; [
+            cacert
+            coreutils
+            bashInteractive
+            self'.packages.osrm-data
+            self'.packages.osrm-server
+            osrm-backend
+          ];
+          /* pathsToLink = [
               "/bin"
               "/opt"
             ];
             */
-          };
         };
       };
     };
+  };
 }
