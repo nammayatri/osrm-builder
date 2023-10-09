0d2b06acb58fc870fe5f9eb0f5b4851304fabfa5
saimanohar.veeravajhula <saimanohar.veeravajhula@juspay.in>
2023-10-09 20:53:42 +0530

    it works

--- osrm.nix
+++ osrm.nix
@@ -3,27 +3,30 @@
 let
   openStreetDataFileName = "SE-zone-latest";
   carLuaPath = ./car.lua; # Adjust this path as needed
-  #tempDir = "/tmp/osrm-temp"; # Use a specific temporary directory path
-  #copyDir = "${tempDir}/osrm-backend-copy"; # Path to the temporary copy directory
 in
 {
-  perSystem = { pkgs, lib, ... }: {
+  perSystem = { self', pkgs, lib, ... }: {
     packages =
       rec {
+        osrm-backend = pkgs.runCommandNoCC
+          "osrm-backend-patched"
+          {}
+          ''
+          mkdir $out
+          cp -r ${pkgs.osrm-backend}/* $out/
+          chmod -R u+w $out/
+          cp ${carLuaPath} $out/share/osrm/profiles/car.lua
+          '';
+
         osrm-data =
           pkgs.runCommandNoCC "osrm-data"
-            { buildInputs = [ pkgs.osrm-backend ]; }
+            { buildInputs = [ self'.packages.osrm-backend ]; }
             ''
+              mkdir $out && cd $out
               ln -s ${inputs.southern-zone-latest} a.osm.pbf
               ln -s ${inputs.eastern-zone-latest} b.osm.pbf
-              mkdir -p $out/share/osrm/profiles
-
-              cp -r ${pkgs.osrm-backend}/* $out/
-
-              # ln -sf ${carLuaPath} $out/share/osrm/profiles/car.lua
-
-              ${pkgs.osmium-tool}/bin/osmium merge -o ${openStreetDataFileName}.osm.pbf a.osm.pbf b.osm.pbf 
-              osrm-extract -p $out/share/osrm/profiles/car.lua ${openStreetDataFileName}.osm.pbf
+              ${pkgs.osmium-tool}/bin/osmium merge -o ${openStreetDataFileName}.osm.pbf a.osm.pbf b.osm.pbf
+              osrm-extract -p ${pkgs.osrm-backend}/share/osrm/profiles/car.lua ${openStreetDataFileName}.osm.pbf
               osrm-partition ${openStreetDataFileName}.osrm
               osrm-customize ${openStreetDataFileName}.osrm
             '';
