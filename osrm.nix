8eb8c557b9ef11f42d0cf9462e369625b3aed644
saimanohar.veeravajhula <saimanoharveeravajhula@gmail.com>
2025-03-10 19:08:48 +0530

    backend/ci/make-custom-fork-osrm-compatible

--- osrm.nix
+++ osrm.nix
@@ -10,32 +10,68 @@ in
   perSystem = { self', pkgs, lib, ... }: {
     packages =
       rec {
-        # Patch osrm-backend to use our own car.lua file
-        osrm-backend = pkgs.osrm-backend.overrideAttrs (oa: {
-          # TODO: Use `patches` to directly patch the car.lua file.
-          # Or, use a fork of osrm-backend, and point to it.
-          src = pkgs.runCommandNoCC "osrm-backend-patched-src" { } ''
-            cp -r ${oa.src} $out
-            chmod -R u+w $out
+        patched-osrm-backend = pkgs.stdenv.mkDerivation {
+          name = "patched-osrm-backend";
+          src = inputs.osrm-backend;
+          buildInputs = [
+            pkgs.cmake
+            pkgs.boost
+            pkgs.tbb_2021_8 # Use TBB 2021.8 explicitly
+            pkgs.expat
+            pkgs.bzip2
+            pkgs.libzip
+            pkgs.pkg-config
+            pkgs.lua5_2
+          ];
+
+          phases = [ "unpackPhase" "buildPhase" "installPhase" ];
+          buildPhase = ''
+            mkdir build
+            cd build
+            cmake .. \
+              -DENABLE_MASON=ON \
+              -DENABLE_IPO=OFF \
+              -DTBB_ROOT=${pkgs.tbb_2021_8} \
+              -DCMAKE_PREFIX_PATH="${pkgs.boost};${pkgs.tbb_2021_8};${pkgs.expat};${pkgs.bzip2};${pkgs.lua5_2}" \
+              -DCMAKE_CXX_STANDARD=20 \
+              -DCMAKE_CXX_FLAGS="-std=c++20 -Wno-error=array-bounds -fpermissive" \
+              -DCMAKE_INSTALL_PREFIX=$out
+            make
+          '';
+          installPhase = ''
+            mkdir -p $out/bin $out/profiles
+            cp osrm-* $out/bin/
+            
+            cp -r ../profiles/* $out/profiles/
+            
             cp ${carLuaPath} $out/profiles/car.lua
           '';
-        });
+        };
 
-        osrm-data =
-          pkgs.runCommandNoCC "osrm-data"
-            { buildInputs = [ self'.packages.osrm-backend ]; }
-            ''
-              mkdir $out && cd $out
-              ln -s ${inputs.india-latest} ${openStreetDataFileName}.osm.pbf
-              ln -s ${speedDataPath} ${indiaSpeedDataFileName}.csv
-              osrm-extract -p ${self'.packages.osrm-backend}/share/osrm/profiles/car.lua ${openStreetDataFileName}.osm.pbf
-              osrm-partition ${openStreetDataFileName}.osrm
-              osrm-customize --segment-speed-file ${indiaSpeedDataFileName}.csv ${openStreetDataFileName}.osrm
-            '';
+        osrm-data = pkgs.runCommandNoCC "osrm-data"
+          { buildInputs = [ patched-osrm-backend ]; }
+          ''
+            mkdir -p $out
+            cd $out
+
+            ln -s ${inputs.india-latest} ${openStreetDataFileName}.osm.pbf
+            ln -s ${speedDataPath} ${indiaSpeedDataFileName}.csv
+
+            ${patched-osrm-backend}/bin/osrm-extract \
+              -p ${patched-osrm-backend}/profiles/car.lua \
+              ${openStreetDataFileName}.osm.pbf
+
+            ${patched-osrm-backend}/bin/osrm-partition \
+              ${openStreetDataFileName}.osrm
+
+            ${patched-osrm-backend}/bin/osrm-customize \
+              --segment-speed-file ${indiaSpeedDataFileName}.csv \
+              ${openStreetDataFileName}.osrm
+          '';
 
         osrm-server = pkgs.writeShellApplication {
           name = "osrm-server";
-          runtimeInputs = [ self'.packages.osrm-backend ];
+          runtimeInputs = [ patched-osrm-backend ];
           text = ''
             set -x
             osrm-routed --algorithm mld \
